7994e422bbb4a3f29778e06ff4f58f694bf84899
Damir Emirzakov <emdamir2@gmail.com>
2025-10-11 17:19:34 -0400

    ADD abstaract CryptoAction class

--- src/main/java/com/javarush/emirzakov/service/CryptoAction.java
+++ src/main/java/com/javarush/emirzakov/service/CryptoAction.java
@@ -1,5 +1,14 @@
 package com.javarush.emirzakov.service;
 
-abstract class CryptoAction {
+public abstract class CryptoAction {
 
+    protected Alphabet alphabet = new Alphabet();
+
+    protected char shiftChar(char c, int key) {
+        int index = alphabet.getCharIndex(c);
+        if (index == -1) return c;
+        return alphabet.getCharByIndex(index + key);
+    }
+
+    public abstract String execute(String text, int key);
 }
